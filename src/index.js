f637476fcc859ab6d99551ff8bb6172c9f0edbe7
Guilherme <guiasis@hotmail.com>
2020-01-15 18:26:22 -0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import './config/ReactotronConfig';
+
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import App from './App';
+
+import { persistor, store } from './store';
+
+export default function Index() {
+  return (
+    <>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+          <App />
+        </PersistGate>
+      </Provider>
+    </>
+  );
+}
